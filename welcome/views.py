8b583913f11870f3870e5f11b49269e459db5e17
Matteo Centonza <matteo.centonza@buongiorno.com>
2018-03-24 16:52:30 +0100

    Testing logging

--- welcome/views.py
+++ welcome/views.py
@@ -1,5 +1,4 @@
 import os
-import logging
 from django.shortcuts import render
 from django.conf import settings
 from django.http import HttpResponse
@@ -13,9 +12,6 @@ def index(request):
     hostname = os.getenv('HOSTNAME', 'unknown')
     PageView.objects.create(hostname=hostname)
    
-    logging.warning('Watch out!')  # will print a message to the console
-    logging.info('I told you so')  # will not print anything
-    
     return render(request, 'welcome/index.html', {
         'hostname': hostname,
         'database': database.info(),
