33b118e0c07814437bdda55a325e72f32b5daa45
Matteo Centonza <matteo.centonza@buongiorno.com>
2018-03-25 04:39:30 +0200

    Tentative test level change

--- welcome/views.py
+++ welcome/views.py
@@ -10,12 +10,17 @@ from .models import PageView
 
 # Get an instance of a logger
 root = logging.getLogger()
+rooterr = logging.getLogger()
 root.setLevel(logging.DEBUG)
+rooterr.setLevel(logging.ERROR)
 
 ch = logging.StreamHandler(sys.stdout)
+cherr = logging.StreamHandler(sys.stdout) 
 ch.setLevel(logging.DEBUG)
+ch.setLevel(logging.ERROR)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 ch.setFormatter(formatter)
+cherr.setFormatter(formatter)
 root.addHandler(ch)
 
 # Create your views here.
@@ -24,10 +29,7 @@ def index(request):
     hostname = os.getenv('HOSTNAME', 'unknown')
     PageView.objects.create(hostname=hostname)
    
-
-    root.error('Something went wrong! (err)')
-    root.info('Something went good! (info)')
-    root.warn('Something went warn! (warn)')
+    rooterr('Something went warn! (warn)')
 
     return render(request, 'welcome/index.html', {
         'hostname': hostname,
@@ -36,7 +38,7 @@ def index(request):
     })
 
 def health(request):
-    root.error('Something went wrong! (err)')
+    root.err('Something went wrong! (err)')
     root.info('Something went good! (info)')
-    root.warn('Something went warn! (warn)')
+    root.warning('Something went warn! (warn)')
     return HttpResponse(PageView.objects.count())
