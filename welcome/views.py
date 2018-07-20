6d6eb07b5a6767dd249c7a24ee8b8db34e107bea
Matteo Centonza <matteo.centonza@docomodigital.com>
2018-07-20 17:24:50 +0200

    Change to stdout

--- welcome/views.py
+++ welcome/views.py
@@ -12,7 +12,7 @@ from .models import PageView
 root = logging.getLogger()
 root.setLevel(logging.DEBUG)
 
-ch = logging.StreamHandler(sys.stderr)
+ch = logging.StreamHandler(sys.stdout)
 ch.setLevel(logging.DEBUG)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 ch.setFormatter(formatter)
